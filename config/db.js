5df47c8c04288eec1a18d6f8760e620c09bb40d0
Nico Brooks <nico@twooctobers.com>
2015-11-04 21:18:36 -0700

    Another DB environment variable update

--- config/db.js
+++ config/db.js
@@ -2,5 +2,5 @@
  * Created by nicobrooks on 10/26/15.
  */
 module.exports = {
-    url : (process.env.MONGOLAB_URI || 'mongodb://localhost') + '/to'
+    url : (process.env.MONGOLAB_URI || 'mongodb://localhost/to')
 }
