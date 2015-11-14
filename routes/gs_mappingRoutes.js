5c138e63ec579c5e2c9ba5292d359598e9a37aed
Nico Brooks <nico@twooctobers.com>
2015-11-13 18:48:54 -0700

    Changed /api/gs/mappings to bulk update on POST

--- routes/gs_mappingRoutes.js
+++ routes/gs_mappingRoutes.js
@@ -6,11 +6,22 @@ module.exports = function(router){
 // ----------------------------------------------------
     router.route('/gs/mappings')
         .post(function(req, res) {
+            /*
             Gs_mapping.create(req.body, function(err) {
                 if (err) {res.send(err); return}
                 res.json({ message: 'records inserted' });
             });
-
+            */
+            var bulk = Gs_mapping.collection.initializeOrderedBulkOp();
+            var body = req.body;
+            for (var i = 0,x = body.length;i<x;i++){
+                var query = {property : body[i].property};
+                bulk.find(query).upsert().updateOne(body[i]);
+            }
+            bulk.execute(function(err) {
+                if (err) {res.send(err); return}
+                res.json({ message: 'records updated' });
+            });
         })
         .get(function(req, res) {
             Gs_mapping.find(function(err, gs_mappings) {
